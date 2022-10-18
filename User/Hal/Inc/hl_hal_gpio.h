edb4e53690f1895bcb2469b86f95f341d2bed111
libo1 <rd46@hollyland-tech.com>
2022-10-18 10:41:00 +0800

    ✏️ 修改gpio（HAL）：更新hal层gpio代码 1.修改gpio代码 2.增加test 结束

--- User/Hal/Inc/hl_hal_gpio.h
+++ User/Hal/Inc/hl_hal_gpio.h
@@ -16,33 +16,18 @@
 /* Exported types ------------------------------------------------------------*/
 typedef enum _HL_GPIO_PORT_E
 {
-    GPIO_USB_CC = 0,  // high（device），low（host）
-    GPIO_CHG_STAT,    // 充电状态检测
-    GPIO_VBUS_DET,    // 是否插入充电器检测
+    GPIO_HALL_TX2 = 0,
+    GPIO_HALL_BOX,
+    GPIO_HALL_TX1,
+    GPIO_HALL_RX,
+    GPIO_CH_INT_N,
+    GPIO_GAUGE_INT,
 
-    GPIO_TX1_OCB,  // 过流检测
-    GPIO_TX2_OCB,
-    GPIO_RX_OCB,
-
-    GPIO_PWR_5V_EN,   // 升压控制
-    GPIO_TX1_PWR_EN,  // 过流芯片控制
-    GPIO_TX2_PWR_EN,
-    GPIO_RX_PWR_EN,
-
-    GPIO_PWR_1V8_EN,  // 1.8V电压输出
-    GPIO_SN1_EN,      // 串口电平转换
-    GPIO_SN2_EN,
-
-    GPIO_USB_OTG,  // 充电芯片控制（host、device）
-    GPIO_LED_R,
-    GPIO_LED_G,
-    GPIO_LED_B,
-
-    GPIO_CHARGE_ENABLE,  // 充电控制
-
-    GPIO_WAKEUP_TX1,  // TX1唤醒
-    GPIO_WAKEUP_TX2,  // TX2唤醒
-    GPIO_WAKEUP_RX,   // RX唤醒
+    GPIO_1V8_EN,
+    GPIO_BOOST_EN,
+    GPIO_RX_POW_EN,
+    GPIO_TX1_POW_EN,
+    GPIO_TX2_POW_EN,
 
     USER_GPIO_NUMBER,
 } HL_GPIO_PORT_E;
@@ -50,19 +35,96 @@ typedef enum _HL_GPIO_PORT_E
 /* Exported constants --------------------------------------------------------*/
 /* Exported macros -----------------------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
-// @brief This function init a gpio with a specified enum value.
+/**
+ * @brief 时钟初始化
+ * @date 2022-10-18
+ * @author libo (rd46@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-18      <td>libo     <td>新建
+ * </table>
+ */
+rt_err_t hl_hal_gpio_clock_init(void);
+
+/**
+ * @brief gpio初始化
+ * @param [in] gpio_index 
+ * @return rt_err_t 
+ * @date 2022-10-18
+ * @author libo (rd46@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-18      <td>libo     <td>新建
+ * </table>
+ */
 rt_err_t hl_hal_gpio_init(HL_GPIO_PORT_E gpio_index);
 
-// @brief This function deinit a gpio with a specified enum value.
+/**
+ * @brief gpio去初始化
+ * @param [in] gpio_index 
+ * @return rt_err_t 
+ * @date 2022-10-18
+ * @author libo (rd46@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-18      <td>libo     <td>新建
+ * </table>
+ */
 rt_err_t hl_hal_gpio_deinit(HL_GPIO_PORT_E gpio_index);
 
-// @brief This function set a gpio with a specified enum value.
+/**
+ * @brief 拉高gpio
+ * @param [in] gpio_index 
+ * @date 2022-10-18
+ * @author libo (rd46@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-18      <td>libo     <td>新建
+ * </table>
+ */
 void hl_hal_gpio_high(HL_GPIO_PORT_E gpio_index);
 
-// @brief This function reset a gpio with a specified enum value.
+/**
+ * @brief 拉低gpio
+ * @param [in] gpio_index 
+ * @date 2022-10-18
+ * @author libo (rd46@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-18      <td>libo     <td>新建
+ * </table>
+ */
 void hl_hal_gpio_low(HL_GPIO_PORT_E gpio_index);
 
-// @brief This function read a gpio with a specified enum value.
+/**
+ * @brief 读取gpio电平
+ * @param [in] gpio_index 
+ * @return int 
+ * @date 2022-10-18
+ * @author libo (rd46@hollyland-tech.com)
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-18      <td>libo     <td>新建
+ * </table>
+ */
 int hl_hal_gpio_read(HL_GPIO_PORT_E gpio_index);
 
 #endif /* _HAL_INC_HL_HAL_GPIO_H_ */
