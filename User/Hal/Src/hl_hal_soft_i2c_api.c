b21ff0d4c5fa1d848ff2e429b5600ea6e7733de7
李林 <lin.li@hollyland-tech.com>
2022-10-19 10:25:09 +0800

    ⚡️ 优化代码（drv、hal）：优化RAM内存   1、通过将全局变量变为只读属性（const）来减少读写数据内存，读写数据占用的是RAM的空间。 结束

--- User/Hal/Src/hl_hal_soft_i2c_api.c
+++ User/Hal/Src/hl_hal_soft_i2c_api.c
@@ -30,9 +30,40 @@
 /* define --------------------------------------------------------------------*/
 /* variables -----------------------------------------------------------------*/
 
-static soft_i2c_info i2c1_info = { 0 };
-static soft_i2c_info i2c2_info = { 0 };
-static soft_i2c_info i2c3_info = { 0 };
+static void hl_hal_us_delay(uint16_t us);
+
+static const soft_i2c_info i2c1_info = {
+    9,           //sda_pin_num
+    8,           //scl_pin_num
+    GPIO_PIN_9,  //gpio_pin_sda
+    GPIO_PIN_8,  //gpio_pin_scl
+    GPIOB_BASE,  //gpiox_base
+    RCC_APB2_PERIPH_GPIOB,
+    GPIOB,           //gpiox
+    hl_hal_us_delay  //微秒级延时函数名
+};
+
+static const soft_i2c_info i2c2_info = {
+    10,           //sda_pin_num
+    9,            //scl_pin_num
+    GPIO_PIN_10,  //gpio_pin_sda
+    GPIO_PIN_9,   //gpio_pin_scl
+    GPIOA_BASE,   //gpiox_base
+    RCC_APB2_PERIPH_GPIOA,
+    GPIOA,           //gpiox
+    hl_hal_us_delay  //微秒级延时函数名
+};
+
+static const soft_i2c_info i2c3_info = {
+    2,           //sda_pin_num
+    3,           //scl_pin_num
+    GPIO_PIN_2,  //gpio_pin_sda
+    GPIO_PIN_3,  //gpio_pin_scl
+    GPIOA_BASE,  //gpiox_base
+    RCC_APB2_PERIPH_GPIOA,
+    GPIOA,           //gpiox
+    hl_hal_us_delay  //微秒级延时函数名
+};
 
 static rt_mutex_t i2c1_mutex = RT_NULL;
 static rt_mutex_t i2c2_mutex = RT_NULL;
@@ -83,17 +114,8 @@ int hl_hal_soft_i2c_api_init(hl_hal_soft_i2c_numb_e i2cx)
                 break;
             }
 
-            i2c1_info.sda_pin_num      = 9;
-            i2c1_info.scl_pin_num      = 8;
-            i2c1_info.gpio_pin_sdax    = GPIO_PIN_9;
-            i2c1_info.gpio_pin_sclx    = GPIO_PIN_8;
-            i2c1_info.gpiox_base       = GPIOB_BASE;
-            i2c1_info.rcc_periph_gpiox = RCC_APB2_PERIPH_GPIOB;
-            i2c1_info.gpiox            = GPIOB;
-            i2c1_info.delay_us         = hl_hal_us_delay;
-
             /* I2C引脚初始化，并使能I2C引脚对应的GPIO组外设时钟 */
-            hl_hal_soft_i2c_init(&i2c1_info);
+            hl_hal_soft_i2c_init((soft_i2c_info*)&i2c1_info);
             i2c1_mutex = rt_mutex_create("i2c1_mutex", RT_IPC_FLAG_PRIO);
 
             i2c1_init_flag = true;
@@ -103,17 +125,8 @@ int hl_hal_soft_i2c_api_init(hl_hal_soft_i2c_numb_e i2cx)
                 break;
             }
 
-            i2c2_info.sda_pin_num      = 10;
-            i2c2_info.scl_pin_num      = 9;
-            i2c2_info.gpio_pin_sdax    = GPIO_PIN_10;
-            i2c2_info.gpio_pin_sclx    = GPIO_PIN_9;
-            i2c2_info.gpiox_base       = GPIOA_BASE;
-            i2c2_info.rcc_periph_gpiox = RCC_APB2_PERIPH_GPIOA;
-            i2c2_info.gpiox            = GPIOA;
-            i2c2_info.delay_us         = hl_hal_us_delay;
-
             /* I2C引脚初始化，并使能I2C引脚对应的GPIO组外设时钟 */
-            hl_hal_soft_i2c_init(&i2c2_info);
+            hl_hal_soft_i2c_init((soft_i2c_info*)&i2c2_info);
             i2c2_mutex = rt_mutex_create("i2c2_mutex", RT_IPC_FLAG_PRIO);
 
             i2c2_init_flag = true;
@@ -122,18 +135,9 @@ int hl_hal_soft_i2c_api_init(hl_hal_soft_i2c_numb_e i2cx)
             if (i2c3_init_flag == true) {
                 break;
             }
-            
-            i2c3_info.sda_pin_num      = 2;
-            i2c3_info.scl_pin_num      = 3;
-            i2c3_info.gpio_pin_sdax    = GPIO_PIN_2;
-            i2c3_info.gpio_pin_sclx    = GPIO_PIN_3;
-            i2c3_info.gpiox_base       = GPIOA_BASE;
-            i2c3_info.rcc_periph_gpiox = RCC_APB2_PERIPH_GPIOA;
-            i2c3_info.gpiox            = GPIOA;
-            i2c3_info.delay_us         = hl_hal_us_delay;
 
             /* I2C引脚初始化，并使能I2C引脚对应的GPIO组外设时钟 */
-            hl_hal_soft_i2c_init(&i2c3_info);
+            hl_hal_soft_i2c_init((soft_i2c_info*)&i2c3_info);
             i2c3_mutex = rt_mutex_create("i2c3_mutex", RT_IPC_FLAG_PRIO);
 
             i2c3_init_flag = true;
@@ -153,7 +157,7 @@ int hl_hal_soft_i2c_api_deinit(hl_hal_soft_i2c_numb_e i2cx)
                 break;
             }
 
-            hl_hal_soft_i2c_deinit(&i2c1_info);
+            hl_hal_soft_i2c_deinit((soft_i2c_info*)&i2c1_info);
             rt_mutex_delete(i2c1_mutex);
 
             i2c1_init_flag = false;
@@ -163,7 +167,7 @@ int hl_hal_soft_i2c_api_deinit(hl_hal_soft_i2c_numb_e i2cx)
                 break;
             }
 
-            hl_hal_soft_i2c_deinit(&i2c2_info);
+            hl_hal_soft_i2c_deinit((soft_i2c_info*)&i2c2_info);
             rt_mutex_delete(i2c2_mutex);
 
             i2c2_init_flag = false;
@@ -173,7 +177,7 @@ int hl_hal_soft_i2c_api_deinit(hl_hal_soft_i2c_numb_e i2cx)
                 break;
             }
 
-            hl_hal_soft_i2c_deinit(&i2c3_info);
+            hl_hal_soft_i2c_deinit((soft_i2c_info*)&i2c3_info);
             rt_mutex_delete(i2c3_mutex);
 
             i2c3_init_flag = false;
@@ -200,7 +204,7 @@ int hl_hal_soft_i2c_api_read(hl_hal_soft_i2c_numb_e i2cx, uint8_t dev_addr, uint
             }
 
             rt_mutex_take(i2c1_mutex, RT_WAITING_FOREVER);
-            hl_hal_soft_i2c_read(&i2c1_info, &i2c_addr, pData, 1);
+            hl_hal_soft_i2c_read((soft_i2c_info*)&i2c1_info, &i2c_addr, pData, 1);
             rt_mutex_release(i2c1_mutex);
         } break;
         case HL_HAL_SOFT_I2C_NUMB_2: {
@@ -209,7 +213,7 @@ int hl_hal_soft_i2c_api_read(hl_hal_soft_i2c_numb_e i2cx, uint8_t dev_addr, uint
             }
 
             rt_mutex_take(i2c2_mutex, RT_WAITING_FOREVER);
-            hl_hal_soft_i2c_read(&i2c2_info, &i2c_addr, pData, 1);
+            hl_hal_soft_i2c_read((soft_i2c_info*)&i2c2_info, &i2c_addr, pData, 1);
             rt_mutex_release(i2c2_mutex);
         } break;
         case HL_HAL_SOFT_I2C_NUMB_3: {
@@ -218,7 +222,7 @@ int hl_hal_soft_i2c_api_read(hl_hal_soft_i2c_numb_e i2cx, uint8_t dev_addr, uint
             }
 
             rt_mutex_take(i2c3_mutex, RT_WAITING_FOREVER);
-            hl_hal_soft_i2c_read(&i2c3_info, &i2c_addr, pData, 1);
+            hl_hal_soft_i2c_read((soft_i2c_info*)&i2c3_info, &i2c_addr, pData, 1);
             rt_mutex_release(i2c3_mutex);
         } break;
         default:
@@ -243,7 +247,7 @@ int hl_hal_soft_i2c_api_write(hl_hal_soft_i2c_numb_e i2cx, uint8_t dev_addr, uin
             }
 
             rt_mutex_take(i2c1_mutex, RT_WAITING_FOREVER);
-            hl_hal_soft_i2c_write(&i2c1_info, &i2c_addr, pData, 1);
+            hl_hal_soft_i2c_write((soft_i2c_info*)&i2c1_info, &i2c_addr, pData, 1);
             rt_mutex_release(i2c1_mutex);
         } break;
         case HL_HAL_SOFT_I2C_NUMB_2: {
@@ -252,16 +256,16 @@ int hl_hal_soft_i2c_api_write(hl_hal_soft_i2c_numb_e i2cx, uint8_t dev_addr, uin
             }
 
             rt_mutex_take(i2c2_mutex, RT_WAITING_FOREVER);
-            hl_hal_soft_i2c_write(&i2c2_info, &i2c_addr, pData, 1);
+            hl_hal_soft_i2c_write((soft_i2c_info*)&i2c2_info, &i2c_addr, pData, 1);
             rt_mutex_release(i2c2_mutex);
         } break;
-        case HL_HAL_SOFT_I2C_NUMB_3: {            
+        case HL_HAL_SOFT_I2C_NUMB_3: {
             if (i2c3_init_flag != true) {
                 return SOFT_I2C_API_FUNC_RET_ERR;
             }
 
             rt_mutex_take(i2c3_mutex, RT_WAITING_FOREVER);
-            hl_hal_soft_i2c_write(&i2c3_info, &i2c_addr, pData, 1);
+            hl_hal_soft_i2c_write((soft_i2c_info*)&i2c3_info, &i2c_addr, pData, 1);
             rt_mutex_release(i2c3_mutex);
         } break;
         default:
