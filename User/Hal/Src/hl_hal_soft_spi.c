5d0a51015f9ad3f72cc5fe9f1b785b1406e44892
jiujun <jiujun.yi@hollyland-tech.com>
2022-10-28 14:28:21 +0800

    优化代码（Hal）：优化软件SPI的读写  1、给软件SPI的读写上锁  结束

--- User/Hal/Src/hl_hal_soft_spi.c
+++ User/Hal/Src/hl_hal_soft_spi.c
@@ -28,6 +28,9 @@
 /* typedef -------------------------------------------------------------------*/
 /* define --------------------------------------------------------------------*/
 /* variables -----------------------------------------------------------------*/
+
+static struct rt_mutex spi_mutex = { 0 };
+
 /* Private function(only *.c)  -----------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
 
@@ -50,6 +53,7 @@
 uint8_t hl_hal_soft_spi_send_recv(hl_hal_soft_spi_info *spi_info, uint8_t wdata)
 {
     uint8_t count, reg_val = 0;
+    rt_mutex_take(&spi_mutex, RT_WAITING_FOREVER);       
     for (count = 0; count < 8; count++) {
         SPI_SCK(spi_info->spi_sck_pin_num, spi_info->gpiox_base) = 0;
         if (wdata & 0x80) {     //在下降沿处，主机发送一位数据给从机
@@ -64,6 +68,7 @@ uint8_t hl_hal_soft_spi_send_recv(hl_hal_soft_spi_info *spi_info, uint8_t wdata)
             reg_val |= 0x01;
         }              
     }
+    rt_mutex_release(&spi_mutex);
     return reg_val;
 }
 #else
@@ -126,6 +131,8 @@ void hl_hal_soft_spi_init(hl_hal_soft_spi_info *spi_info)
     gpio_init_struct.GPIO_Mode = GPIO_Mode_Input;
     gpio_init_struct.GPIO_Pull = GPIO_Pull_Up;
     GPIO_InitPeripheral(spi_info->gpiox, &gpio_init_struct);
+
+    rt_mutex_init(&spi_mutex, "spi_mutex", RT_IPC_FLAG_PRIO);
 }
 
 void hl_hal_soft_spi_deinit(hl_hal_soft_spi_info *spi_info)
@@ -139,6 +146,8 @@ void hl_hal_soft_spi_deinit(hl_hal_soft_spi_info *spi_info)
     gpio_init_struct.GPIO_Pull = GPIO_No_Pull;
     gpio_init_struct.GPIO_Slew_Rate = GPIO_Slew_Rate_Low;
     GPIO_InitPeripheral(spi_info->gpiox, &gpio_init_struct);
+
+    rt_mutex_detach(&spi_mutex);
 }
 /*
  * EOF
