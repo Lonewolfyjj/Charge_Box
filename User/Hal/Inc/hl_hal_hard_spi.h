cca6ddf4a2fd8502ab0d1727f7a9748313ed36a7
jiujun <jiujun.yi@hollyland-tech.com>
2022-12-05 20:46:06 +0800

    添加代码（Hal）：添加Hal层硬件SPI代码   1、增加Hardware SPI代码，已验证   结束 修改代码（Driver）：修改FLASH驱动代码   1、修改FLASH的驱动代码，改成软件SPI和硬件SPI可以选择使用，只要改宏定义即可，已验证   2、添加写使能等待超时功能（计数），已验证   3、添加写成功等待超时功能（计数），已验证   结束

--- User/Hal/Inc/hl_hal_hard_spi.h
+++ User/Hal/Inc/hl_hal_hard_spi.h
@@ -1,9 +1,9 @@
 /**
- * @file hl_drv_flash.h
+ * @file spi_flash.h
  * @author yijiujun (jiujun.yi@hollyland-tech.com)
  * @brief 
  * @version V1.0
- * @date 2022-10-25
+ * @date 2022-11-29
  * 
  * ██╗  ██╗ ██████╗ ██╗     ██╗  ██╗   ██╗██╗      █████╗ ███╗   ██╗██████╗ 
  * ██║  ██║██╔═══██╗██║     ██║  ╚██╗ ██╔╝██║     ██╔══██╗████╗  ██║██╔══██╗
@@ -16,48 +16,64 @@
  * @par 修改日志:
  * <table>
  * <tr><th>Date           <th>Version  <th>Author         <th>Description
- * <tr><td>2022-10-25     <td>v1.0     <td>yijiujun     <td>内容
+ * <tr><td>2022-11-29     <td>v1.0     <td>yijiujun     <td>内容
  * </table>
  * 
  */ 
 /* Define to prevent recursive inclusion -------------------------------------*/
 
-#ifndef __HL_DRV_FLASH_H__
-#define __HL_DRV_FLASH_H__
+#ifndef _HL_HAL_HARD_SPI_H_
+#define _HL_HAL_HARD_SPI_H_
 
 /* Includes ------------------------------------------------------------------*/
 
-#include "hl_hal_soft_spi.h"
-#include "hl_hal_hard_spi.h"
+#include "n32l40x.h"
 
 /* typedef -------------------------------------------------------------------*/
-
 /* define --------------------------------------------------------------------*/
 
-/* FLASH的驱动类型，有软件模拟SPI驱动（HL_DRV_FLASH_TYPE = 0）| 硬件SPI驱动（HL_DRV_FLASH_TYPE = 1） */
-#define HL_DRV_FLASH_TYPE       1
+#define FLASH_SPI                SPI2
+#define FLASH_SPI_CLK            RCC_APB2_PERIPH_SPI2
 
-//#define W25XXX_ID               0xEF3015    // W25X16
-//#define W25XXX_ID               0xEF4015    // W25Q16
-//#define W25XXX_ID               0xEF4018    // W25Q128
-#define W25XXX_ID               0xef4016    //W25Q32
+//SCK引脚
+#define FLASH_SPI_SCK_PIN        GPIO_PIN_13 
+#define FLASH_SPI_SCK_GPIO_PORT  GPIOB      
+#define FLASH_SPI_SCK_GPIO_CLK   RCC_APB2_PERIPH_GPIOB
 
-#define debug_printf            rt_kprintf
-#define FLASH_RET_OK            0
-#define FLASH_RET_ERR           1
+//MISO引脚
+#define FLASH_SPI_MISO_PIN       GPIO_PIN_14 
+#define FLASH_SPI_MISO_GPIO_PORT GPIOB      
+#define FLASH_SPI_MISO_GPIO_CLK  RCC_APB2_PERIPH_GPIOB
+
+//MOSI引脚
+#define FLASH_SPI_MOSI_PIN       GPIO_PIN_15 
+#define FLASH_SPI_MOSI_GPIO_PORT GPIOB      
+#define FLASH_SPI_MOSI_GPIO_CLK  RCC_APB2_PERIPH_GPIOB
+
+//CS(NSS)引脚 片选选普通GPIO即可
+#define FLASH_CS_PIN             GPIO_PIN_12 
+#define FLASH_CS_GPIO_PORT       GPIOB      
+#define FLASH_CS_GPIO_CLK        RCC_APB2_PERIPH_GPIOB
+
+#define HARD_SPI_CS_LOW()     GPIO_ResetBits(FLASH_CS_GPIO_PORT, FLASH_CS_PIN)
+#define HARD_SPI_CS_HIGH()    GPIO_SetBits(FLASH_CS_GPIO_PORT, FLASH_CS_PIN)
 
 /* variables -----------------------------------------------------------------*/
 /* Private function(only *.c)  -----------------------------------------------*/
 /* Exported functions --------------------------------------------------------*/
 
-int hl_drv_flash_init(void);
-int hl_drv_flash_deinit(void);
-int hl_drv_flash_read(uint32_t addr, uint8_t *r_data, uint32_t len);
-int hl_drv_flash_write(uint32_t addr, uint8_t* w_data, uint32_t len);
-uint8_t hl_drv_flash_erase_sector(uint32_t addr);
-void hl_drv_flash_erase_chip(void);
+void hl_hal_hard_spi_init(void);
+uint8_t hl_hal_hard_spi_send_recv(uint8_t byte);
+void hl_hal_hard_spi_deinit(void);
 
 #endif
 /*
  * EOF
  */
+
+
+
+
+
+
+
---
+++ User/Hal/Src/hl_hal_hard_spi.c
@@ -0,0 +1,133 @@
+/**
+ * @file hl_hal_hard_spi.c
+ * @author yijiujun (jiujun.yi@hollyland-tech.com)
+ * @brief 
+ * @version V1.0
+ * @date 2022-11-29
+ * 
+ * ██╗  ██╗ ██████╗ ██╗     ██╗  ██╗   ██╗██╗      █████╗ ███╗   ██╗██████╗ 
+ * ██║  ██║██╔═══██╗██║     ██║  ╚██╗ ██╔╝██║     ██╔══██╗████╗  ██║██╔══██╗
+ * ███████║██║   ██║██║     ██║   ╚████╔╝ ██║     ███████║██╔██╗ ██║██║  ██║
+ * ██╔══██║██║   ██║██║     ██║    ╚██╔╝  ██║     ██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██║╚██████╔╝███████╗███████╗██║   ███████╗██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ * @copyright Copyright (c) 2022 hollyland
+ * 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date           <th>Version  <th>Author         <th>Description
+ * <tr><td>2022-11-29     <td>v1.0     <td>yijiujun     <td>内容
+ * </table>
+ * 
+ */ 
+/* Define to prevent recursive inclusion -------------------------------------*/
+/* Includes ------------------------------------------------------------------*/
+
+#include "hl_hal_hard_spi.h"
+
+/* typedef -------------------------------------------------------------------*/
+/* define --------------------------------------------------------------------*/
+/* variables -----------------------------------------------------------------*/
+/* Private function(only *.c)  -----------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+void hl_hal_hard_spi_init(void) 
+{
+
+    SPI_InitType spi_type;
+    GPIO_InitType gpio_init_struct;
+
+    GPIO_InitStruct(&gpio_init_struct);
+
+    RCC_EnableAPB2PeriphClk(
+        FLASH_CS_GPIO_CLK | FLASH_SPI_MOSI_GPIO_CLK | FLASH_SPI_MISO_GPIO_CLK | FLASH_SPI_SCK_GPIO_CLK, ENABLE);
+
+    RCC_EnableAPB2PeriphClk(FLASH_SPI_CLK|RCC_APB2_PERIPH_AFIO, ENABLE);
+
+    gpio_init_struct.Pin        = FLASH_SPI_SCK_PIN;
+    gpio_init_struct.GPIO_Mode  = GPIO_Mode_AF_PP;
+    gpio_init_struct.GPIO_Alternate = GPIO_AF0_SPI2;
+    GPIO_InitPeripheral(FLASH_SPI_SCK_GPIO_PORT, &gpio_init_struct);
+
+    /*!< Configure FLASH_CS_PIN pin: FLASH Card CS pin */
+    gpio_init_struct.Pin       = FLASH_CS_PIN;
+    gpio_init_struct.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitPeripheral(FLASH_CS_GPIO_PORT, &gpio_init_struct);
+
+    /*!< Configure FLASH_SPI pins: MOSI */
+    gpio_init_struct.Pin        = FLASH_SPI_MOSI_PIN;
+    gpio_init_struct.GPIO_Mode  = GPIO_Mode_AF_PP;
+    gpio_init_struct.GPIO_Alternate = GPIO_AF0_SPI2;
+    GPIO_InitPeripheral(FLASH_SPI_MOSI_GPIO_PORT, &gpio_init_struct);
+
+    /*!< Configure FLASH_SPI pins: MISO */
+    gpio_init_struct.Pin       = FLASH_SPI_MISO_PIN;
+    gpio_init_struct.GPIO_Mode = GPIO_Mode_Input;
+    GPIO_InitPeripheral(FLASH_SPI_MISO_GPIO_PORT, &gpio_init_struct);
+
+    HARD_SPI_CS_HIGH();
+
+    spi_type.DataDirection = SPI_DIR_DOUBLELINE_FULLDUPLEX;
+    spi_type.SpiMode = SPI_MODE_MASTER;
+    spi_type.DataLen = SPI_DATA_SIZE_8BITS;
+    spi_type.CLKPOL = SPI_CLKPOL_HIGH;
+    spi_type.CLKPHA = SPI_CLKPHA_SECOND_EDGE;
+    spi_type.NSS = SPI_NSS_SOFT;
+    spi_type.BaudRatePres = SPI_BR_PRESCALER_4;
+    spi_type.FirstBit = SPI_FB_MSB;
+    spi_type.CRCPoly = 7;
+    SPI_Init(FLASH_SPI, &spi_type);
+
+    SPI_Enable(FLASH_SPI, ENABLE);
+
+}
+
+uint8_t hl_hal_hard_spi_send_recv(uint8_t byte)
+{
+    /*!< Loop while DAT register in not emplty */
+    while (SPI_I2S_GetStatus(FLASH_SPI, SPI_I2S_TE_FLAG) == RESET);
+
+    /*!< Send byte through the FLASH_SPI peripheral */
+    SPI_I2S_TransmitData(FLASH_SPI, byte);
+
+    /*!< Wait to receive a byte */
+    while (SPI_I2S_GetStatus(FLASH_SPI, SPI_I2S_RNE_FLAG) == RESET);
+
+    /*!< Return the byte read from the SPI bus */
+    return SPI_I2S_ReceiveData(FLASH_SPI);
+}
+
+void hl_hal_hard_spi_deinit(void)
+{
+    GPIO_InitType GPIO_InitStructure;
+
+    /*!< Disable the FLASH_SPI  */
+    SPI_Enable(FLASH_SPI, DISABLE);
+
+    /*!< DeInitializes the FLASH_SPI */
+    SPI_I2S_DeInit(FLASH_SPI);
+
+    /*!< FLASH_SPI Periph clock disable */
+    RCC_EnableAPB2PeriphClk(FLASH_SPI_CLK, DISABLE);
+
+    /*!< Configure FLASH_SPI pins: SCK */
+    GPIO_InitStructure.Pin       = FLASH_SPI_SCK_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Analog;         //模拟模式
+    GPIO_InitPeripheral(FLASH_SPI_SCK_GPIO_PORT, &GPIO_InitStructure);
+
+    /*!< Configure FLASH_SPI pins: MISO */
+    GPIO_InitStructure.Pin = FLASH_SPI_MISO_PIN;
+    GPIO_InitPeripheral(FLASH_SPI_MISO_GPIO_PORT, &GPIO_InitStructure);
+
+    /*!< Configure FLASH_SPI pins: MOSI */
+    GPIO_InitStructure.Pin = FLASH_SPI_MOSI_PIN;
+    GPIO_InitPeripheral(FLASH_SPI_MOSI_GPIO_PORT, &GPIO_InitStructure);
+
+    /*!< Configure FLASH_CS_PIN pin: FLASH Card CS pin */
+    GPIO_InitStructure.Pin = FLASH_CS_PIN;
+    GPIO_InitPeripheral(FLASH_CS_GPIO_PORT, &GPIO_InitStructure);
+}
+
+/*
+ * EOF
+ */
